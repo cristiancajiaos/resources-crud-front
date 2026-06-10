306da5bddc0a615f97698976ec77a9f1dbf36b63
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-10 17:01:45 -0400

    Creado formulario reactivo en vista Users

--- src/app/modules/users/users.ts
+++ src/app/modules/users/users.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEdit, faFloppyDisk, faTimes, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-users',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, FormsModule, ReactiveFormsModule],
   templateUrl: './users.html',
   styleUrl: './users.scss',
 })
-export class Users {
+export class Users implements OnInit {
+
+  private fb = inject(FormBuilder);
+
+  public userForm: FormGroup = new FormGroup({});
+
+  public userName: FormControl = new FormControl('', [Validators.required]);
+  public userUsername: FormControl = new FormControl('', [Validators.required]);
+  public userEmail: FormControl = new FormControl('', [Validators.required, Validators.email]);
 
   public cancelDeleteIcon: IconDefinition = faTimes;
   public saveIcon: IconDefinition = faFloppyDisk;
   public editIcon: IconDefinition = faEdit;
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: this.userName,
+      username: this.userUsername,
+      email: this.userEmail
+    });
+  }
 }
