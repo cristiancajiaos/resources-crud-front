a414d98abfd0b0048856607bead87ad1ebd321ae
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-10 12:37:24 -0400

    Creadas vistas Home y Posts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './modules/home/home';
+import { Posts } from './modules/posts/posts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: 'posts',
+    component: Posts
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
