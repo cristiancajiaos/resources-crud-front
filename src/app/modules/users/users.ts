355ac43045d273cc4882c4fb66ad084966e20c96
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-10 16:50:39 -0400

    Establecido layout de vista Users

--- src/app/modules/users/users.ts
+++ src/app/modules/users/users.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEdit, faFloppyDisk, faTimes, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './users.html',
   styleUrl: './users.scss',
 })
-export class Users {}
+export class Users {
+
+  public cancelDeleteIcon: IconDefinition = faTimes;
+  public saveIcon: IconDefinition = faFloppyDisk;
+  public editIcon: IconDefinition = faEdit;
+}
