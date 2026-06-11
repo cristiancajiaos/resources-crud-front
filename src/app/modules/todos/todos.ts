d67916fd6a24f245873b4d623a7dba481f3b58e5
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-11 17:12:30 -0400

    Creado servicio TodoService, inyectado en componente Todos para obtener todos los Todos

--- src/app/modules/todos/todos.ts
+++ src/app/modules/todos/todos.ts
@@ -1,19 +1,27 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { faPencil, faTimes, faFloppyDisk, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgClass } from '@angular/common';
+import { Todo } from '../../interfaces/todo';
+import { TodoService } from '../../services/todo-service';
+import { Subscription } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-todos',
-  imports: [ToggleSwitchModule, FormsModule, ReactiveFormsModule, FontAwesomeModule, JsonPipe],
+  imports: [ToggleSwitchModule, FormsModule, ReactiveFormsModule, FontAwesomeModule, NgClass],
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
-export class Todos implements OnInit {
+export class Todos implements OnInit, OnDestroy {
 
+  public todoService = inject(TodoService);
   public fb = inject(FormBuilder);
+  public toastr = inject(ToastrService);
+
+  public todos: Todo[] = [];
 
   public todoForm: FormGroup = new FormGroup({});
 
@@ -26,14 +34,35 @@ export class Todos implements OnInit {
   public saveIcon: IconDefinition = faFloppyDisk;
   public editIcon: IconDefinition = faPencil;
 
+  private getTodosSubscription: Subscription = new Subscription();
+
   ngOnInit(): void {
     this.todoForm = this.fb.group({
       todoTitle: this.todoTitle,
       todoCompleted: this.todoCompletedForm
     });
+    this.getAllTodos();
+  }
+
+  private getAllTodos() {
+    this.getTodosSubscription = this.todoService.getAllTodos().subscribe({
+      next: (todos) => {
+        this.todos = todos;
+      },
+      error: (error) => {
+        this.toastr.error('There was an error getting the todos');
+      },
+      complete: () => {}
+    });
   }
 
   public saveOrEditTodo() {
-    
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.getTodosSubscription) {
+      this.getTodosSubscription.unsubscribe();
+    }
   }
 }
---
+++ src/app/services/todo-service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from '../interfaces/todo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+
+  private http = inject(HttpClient);
+
+  public getAllTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>('/todo');
+  }
+}
