3bd0a0c50b8aea6e20117a3c5f2e4c1449a9cab8
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-11 16:21:12 -0400

    Instalados paquetes de PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';  
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { loadingInterceptor } from './interceptors/loading-interceptor';
 import { baseUrlInterceptor } from './interceptors/base-url-interceptor';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideToastr(),
-    provideHttpClient(withInterceptors([baseUrlInterceptor, loadingInterceptor]), withFetch())
+    provideHttpClient(withInterceptors([baseUrlInterceptor, loadingInterceptor]), withFetch()),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
