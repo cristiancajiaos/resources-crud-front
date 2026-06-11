fa88908b3544da7de095b35e40d585074c65f3ab
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-11 16:59:55 -0400

    Creado formulario reactivo para vista Todos

--- src/app/modules/todos/todos.ts
+++ src/app/modules/todos/todos.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { faPencil, faTimes, faFloppyDisk, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [ToggleSwitchModule, FormsModule, ReactiveFormsModule, FontAwesomeModule],
+  imports: [ToggleSwitchModule, FormsModule, ReactiveFormsModule, FontAwesomeModule, JsonPipe],
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
-export class Todos {
+export class Todos implements OnInit {
 
-  public todoCompleted: boolean = false; 
+  public fb = inject(FormBuilder);
+
+  public todoForm: FormGroup = new FormGroup({});
+
+  public todoCompleted: boolean = false;
+
+  public todoTitle: FormControl = new FormControl('', [Validators.required]);
+  public todoCompletedForm: FormControl = new FormControl(false);
 
   public cancelDeleteIcon: IconDefinition = faTimes;
   public saveIcon: IconDefinition = faFloppyDisk;
   public editIcon: IconDefinition = faPencil;
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      todoTitle: this.todoTitle,
+      todoCompleted: this.todoCompletedForm
+    });
+  }
+
+  public saveOrEditTodo() {
+    
+  }
 }
