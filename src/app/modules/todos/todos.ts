88a7fd1db67a2309a996b59c712d8c13ec5341d3
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-11 16:42:35 -0400

    Definido layout de vista Todos

--- src/app/modules/todos/todos.ts
+++ src/app/modules/todos/todos.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { faPencil, faTimes, faFloppyDisk, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [ToggleSwitchModule, FormsModule, ReactiveFormsModule, FontAwesomeModule],
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
-export class Todos {}
+export class Todos {
+
+  public todoCompleted: boolean = false; 
+
+  public cancelDeleteIcon: IconDefinition = faTimes;
+  public saveIcon: IconDefinition = faFloppyDisk;
+  public editIcon: IconDefinition = faPencil;
+}
